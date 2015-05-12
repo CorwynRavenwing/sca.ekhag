4614104c8044d2e5706ce5d3e8e006e0d04e1cb7
Corwyn Ravenwing <herbweb@eastkingdom.org>
2015-05-11 20:23:20 -0400

    gallery

--- connect.php
+++ connect.php
@@ -3,8 +3,9 @@
 
 include "site/connection.php";
 
-$conn = mysql_connect($db_host, $db_user, $db_pass) or die(mysql_error());
-mysql_select_db($db_name, $conn) or die(mysql_error());
+$conn = mysql_connect($db_server, $db_user_name, $db_password)
+            or die(mysql_error());
+mysql_select_db($db_dbname, $conn) or die(mysql_error());
 
 # ------------------------------------------------------------
 # FUNCTION DEFINITIONS SECTION
@@ -13,21 +14,21 @@ mysql_select_db($db_name, $conn) or die(mysql_error());
 function do_query($sql) {
     echo "<!-- SQL:\n$sql\n-->\n";
     $qDo = mysql_query($sql) or die(mysql_error());
-	return $qDo;
+  return $qDo;
 }
 // end function do_query
 
 function count_where($table, $where_clause) {
-	$sql = "SELECT count(*) AS num FROM $table WHERE $where_clause";
+  $sql = "SELECT count(*) AS num FROM $table WHERE $where_clause";
     echo "<!-- SQL:\n$sql\n-->\n";
     $qCount = mysql_query($sql) or die(mysql_error());
-	if (! $qCount) { return 0; }
+  if (! $qCount) { return 0; }
     if ($rsCount = mysql_fetch_array($qCount)) {
-		$num = $rsCount['num'];
-	} else {
-		$num = 0;
-	}
-	return $num;
+    $num = $rsCount['num'];
+  } else {
+    $num = 0;
+  }
+  return $num;
 }
 // end function count_where
 ?>
